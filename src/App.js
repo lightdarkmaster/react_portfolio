251871bcc465705c5297256373ceaa3b722619c1
darkMasterv <79690406+lightdarkmaster@users.noreply.github.com>
2025-01-27 08:36:30 +0800

    Remove Unecessary files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Dashboard from './dashboard';
+import Navbar from './navbar';
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
